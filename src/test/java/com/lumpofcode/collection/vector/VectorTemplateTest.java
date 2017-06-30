5634c7b8f5c150ec3fd5fef1a565cf2bfe609969
Ed Murphy <emurphy@babycenter.com>
2017-06-30 13:17:31 -0700

    - changed VectorTemplate to use a Writer rather than a StringBuilder, so we can write directly to files or System.out - update VectorTemplate to make it simpler and to implement push(e1..e16) and pushAll()

--- src/test/java/com/lumpofcode/collection/vector/VectorTemplateTest.java
+++ src/test/java/com/lumpofcode/collection/vector/VectorTemplateTest.java
@@ -1,8 +1,12 @@
 package com.lumpofcode.collection.vector;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -18,9 +22,47 @@ public class VectorTemplateTest
 		
 		for(int i = 1; i <= Vectors.VECTOR_NODE_SIZE; i += 1)
 		{
-			template.generate(writer, i);
+			template.generate(writer, i, Vectors.VECTOR_NODE_SIZE);
 			final String vectorOf = writer.toString();
 			System.out.println(vectorOf);
+		}
+		
+	}
+	
+	/**
+	 * Run this test manually in order to generate
+	 * the VectorOfNN.java files in
+	 * com/lumpofcode/collection/vector/impl
+	 *
+	 * @throws IOException
+	 */
+	@Test
+	@Ignore
+	public void vectorTemplateWriter() throws IOException
+	{
+		final VectorTemplate template = new VectorTemplate();
+		
+		//
+		// create directory structure './src/generated/java/com/lumpofcode/collection/vector/' starting at './src/generated'
+		//
+//		final File dir = new File("./src/main/java/com/lumpofcode/collection/vector/impl");
+//
+//		// attempt to create the directory here
+//		if(!dir.exists())
+//		{
+//			if (!dir.mkdir())
+//			{
+//				throw new RuntimeException("Unable to create folder for generated sources.");
+//			}
+//		}
+		
+		for(int i = 1; i <= Vectors.VECTOR_NODE_SIZE; i += 1)
+		{
+			final String filePath = "./src/main/java/com/lumpofcode/collection/vector/impl/VectorOf{{size}}.java".replace("{{size}}", String.valueOf(i));
+			final Writer writer = new FileWriter(filePath);
+			template.generate(writer, i, Vectors.VECTOR_NODE_SIZE);
+			writer.flush();
+			writer.close();
 		}
 		
 	}
